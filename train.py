da5248203ab66a8fbd11cf9d1d20e84a503bba72
Kevin24816 <li.qingkai@yahoo.com>
2017-01-04 03:36:02 -0600

    update

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 
-# To train the model, place training corpus in home directory (inside PubMed_LDA folder). Change
-# corpus_filename to that of the corpus file.
+# To train the model, place training corpus in Medline format in home directory (inside PubMed_LDA folder). Change
+# "FILENAME.TXT" to that of the corpus file.
 
 corpus_filename = "FILENAME.txt"
 
 # pre-processing text
 
+# convert corpus to document per line, extract text, then train 
