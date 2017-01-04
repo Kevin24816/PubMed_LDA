7a3cde631952587434a75772e2fee2aa4c34f71e
Kevin24816 <li.qingkai@yahoo.com>
2017-01-04 03:11:17 -0600

    update 2

---
+++ train.py
@@ -0,0 +1,8 @@
+# To train the model, place training corpus in home directory (inside PubMed_LDA folder).
+
+from helper_functions.generate_LDA import *
+
+corpus_filename = "FILENAME.txt"
+
+# pre-processing text
+
