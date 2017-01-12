52b07bd7fba7be3dd4c6a3dced8bb189cf2cf535
Kevin Li <li.qingkai@yahoo.com>
2017-01-12 11:25:16 -0600

    Add files via upload

--- train.py
+++ train.py
@@ -1,10 +1,33 @@
 from __future__ import print_function
+from helper_functions.text_preprocessing import *
+from helper_functions.generate_LDA import *
 
-# To train the model, place training corpus in Medline format in home directory (inside PubMed_LDA folder). Change
-# "FILENAME.TXT" to that of the corpus file.
+# To train the model, place training corpus in Medline format in home directory (inside PubMed_LDA folder). To
+# run the topic model on your own data, change "hepatotoxicity.medline" to that of the corpus file name.
 
-corpus_filename = "FILENAME.txt"
+filename = "hepatotoxicity.medline"
+model_id = "hepatotoxicity_full"
 
 # pre-processing text
+abstract_list = get_abstract_list(filename)
+texts = process_texts(abstract_list)
 
-# convert corpus to document per line, extract text, then train 
+# run model
+parameters = {}
+parameters["parameter_list"] = str(parameters)
+parameters["model_id"] = model_id
+parameters["transformation"] = None
+parameters["update_every"] = 1
+parameters["minimum_probability"] = 0.01
+parameters["alpha"] = 'auto'
+# the following parameters should be changed to optimize the model
+parameters["num_topics"] = 5
+parameters["chunksize"]= 1600
+parameters["passes"] = 20
+parameters["iterations"] = 2000
+
+run_LDA(texts, parameters)
+
+# display topics
+
+
